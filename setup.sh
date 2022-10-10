f6f90e66bb05f390932b06b1a0c552eff0971520
Solmazsoltani <88332157+Solmazsoltani@users.noreply.github.com>
2022-10-10 19:15:58 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set AUTH0_DOMAIN="fsnd-kml.auth0.com"
+set ALGORITHMS="RS256"
+set API_AUDIENCE="capstone"
+
+set DATABASE_URL="postgres:///capstone"
+set FLASK_APP=flaskr
+set FLASK_DEBUG=True
+set FLASK_ENVIRONMENT=debug
